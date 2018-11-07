83bdc02a1ffdba9e9847f1f05554eaafdda7edd9
qilong <2839506427@qq.com>
2018-11-07 14:00:39 +0800

    122

---
+++ 9/14-DOM9999.js
@@ -0,0 +1,22 @@
+
+        //增 删 改 查
+        //增
+        //document.createElement
+        //appendChild
+
+        //删
+        //removeChild
+
+        //改
+        //style
+        //id
+        //className
+        //innerHTML
+        //innerText
+
+        //查
+        //getElementById
+        //getElementsByTagName
+        //getELementsByClassName
+
+    
---
+++ 9/61-promise.js
@@ -0,0 +1,8 @@
+OCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+
+<script src="jquery-3.2.1.min.js">
